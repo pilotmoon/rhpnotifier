f6c91dcd793bdda8c9a79b562b8e79c42f0589e8
Nicholas Moore <nick@reqr.net>
2010-01-19 19:17:53 +0000

    removed guff from top of main.m

--- main.m
+++ main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  rhpchecker
-//
-//  Created by Nicholas Moore on 19/01/2010.
-//  Copyright 2010 Nicholas Moore. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, char *argv[])
